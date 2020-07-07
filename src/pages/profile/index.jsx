db1b310a6c9b72adb730b5ab8b776690ec46e51e
Ben Adams <benkingadams@gmail.com>
2020-07-06 20:24:18 -0400

    Implement user profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -5,23 +5,51 @@ const Profile = () => {
 const profileData = 
 { 
   name: 'Ben Adams',
-  email: 'ben@email', 
+  email: 'ben@email.com', 
   cohort: 1, 
-  role: 'Front-end', 
+  role: 'Front-end Developer', 
   company: 'Genius Plaza', 
-  socialMedia: 'https://linkedin.com/en/BenAdams',
+  socialMedia: 'https://linkedIn.com/en/BenAdams',
   imageSrc: 'https://via.placeholder.com/150'
 }
 
   return (
     <div className={styles.profileContainer}>
       <img className={styles.circleImage}  src={profileData.imageSrc} alt="profile image"/>  
-      <h3>{profileData.name}</h3>
-      <p>{profileData.cohort}</p>
-      <p>{profileData.email}</p>
-      <p>{profileData.role}</p>
-      <p>{profileData.company}</p>
-      <p>{profileData.socialMedia}</p>
+      <section>
+        <dl>
+          <dt>Name</dt>
+          <dd>
+            <p>{profileData.name}</p>
+          </dd>
+
+          <dt>Cohort</dt>
+          <dd>
+            <p>{profileData.cohort}</p>
+          </dd>
+
+          <dt>Email</dt>
+          <dd>
+            <p>{profileData.email}</p>
+          </dd>
+
+          <dt>Role</dt>
+          <dd>
+            <p>{profileData.role}</p>
+          </dd>
+
+          <dt>Company</dt>
+          <dd>
+            <p>{profileData.company}</p>
+          </dd>
+
+          <dt>Social Media</dt>
+          <dd>
+            <p>{profileData.socialMedia}</p>
+          </dd>
+        </dl>  
+          
+      </section>
     </div>
   )
 }
